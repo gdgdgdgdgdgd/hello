50365bbd78b74b530e90b1258161b3babe0899ea
양치열 <mijyukudreamer@naver.com>
2022-05-06 13:40:15 +0900

    commit2

--- src/votejava01/VoteVo.java
+++ src/votejava01/VoteVo.java
@@ -2,4 +2,36 @@ package votejava01;
 
 public class VoteVo {
 
+	private long number;
+	private String name;
+	private long vote;
+	
+	
+	public long getNumber() {
+		return number;
+	}
+	public void setNumber(long number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getVote() {
+		return vote;
+	}
+	public void setVote(long vote) {
+		this.vote = vote;
+	}
+	@Override
+	public String toString() {
+		return "VoteVo [number=" + number + ", name=" + name + ", vote=" + vote + "]";
+	}
+
+	
+	
+	
+	
 }
