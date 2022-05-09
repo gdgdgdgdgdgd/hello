02679edc7a9a91d6af6f8f926ac15f0c3576d705
양치열 <mijyukudreamer@naver.com>
2022-05-09 15:03:05 +0900

    test3

--- src/votejava01/JdbcTemplate.java
+++ src/votejava01/JdbcTemplate.java
@@ -1,5 +1,10 @@
 package votejava01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class JdbcTemplate {
 	private static JdbcTemplate instance;
 	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -14,4 +19,18 @@ public class JdbcTemplate {
 		}
 	}
 	
+	public static JdbcTemplate getInstance() { 
+		//객체생성은 무조건 getInstance만 통해서 생성가능
+		//getInstance를 호출해서 해당(자기자신만의) instance를 반환받도록 하는 것
+		//synchronized(JdbcTemplate.class) {
+			if(instance == null) {
+				instance = new JdbcTemplate();
+			}
+		//}
+		return instance;
+	}
+	
+	public Connection getConnetion() throws SQLException{
+		return DriverManager.getConnection(url, user, password);
+	}
 }
