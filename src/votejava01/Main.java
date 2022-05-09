c4148e750afead1c061ca08f6eed13af9fab701d
양치열 <mijyukudreamer@naver.com>
2022-05-09 16:00:06 +0900

    test4

---
+++ src/votejava01/Main.java
@@ -0,0 +1,10 @@
+package votejava01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		VoteDao voteDao = new VoteDao();
+
+	}
+
+}
--- src/votejava01/VoteDao.java
+++ src/votejava01/VoteDao.java
@@ -1,5 +1,9 @@
 package votejava01;
 
 public class VoteDao {
+	private	JdbcTemplate jdbcTemplate;
 	
+	public VoteDao() {
+		jdbcTemplate = JdbcTemplate.getInstance(); 
+	}
 }
